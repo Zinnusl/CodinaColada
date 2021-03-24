a0bdd88aa05de7117ec6122b07a0432aec775df9
Fahersto <fabian.stotz@yahoo.de>
2021-03-25 00:08:19 +0100

    enable to pause pause engine using imgui

--- CodingColada/src/Engine.cpp
+++ CodingColada/src/Engine.cpp
@@ -20,89 +20,78 @@ Engine::Engine(std::unique_ptr<IRenderer> renderer, std::unique_ptr<IInput> inpu
 
 void Engine::StartGame()
 {
+	auto currentFrame = std::chrono::steady_clock::now();
 	auto lastFrame = std::chrono::steady_clock::now();
 
-	//fps display
-	int32_t fpsTimer = 0;
-	int32_t fps = 0;
-	int32_t fpsDisplay = 0;
-
+	float subframe = 0;
 	//physics
 	const int32_t ticksPerSecond = 64;
 	const int32_t microSecondsPerTick = 1000000 / ticksPerSecond;
 	int32_t timeSinceLastPhysicsTick = 0;
 
+	bool pauseGame = false;
+
 	while (!stopGame)
 	{
-		auto currentFrame = std::chrono::steady_clock::now();
+		//Handle input
+		//needs to be outside to allow to pause game
+		input_->ProcessInput();
+		currentFrame = std::chrono::steady_clock::now();
 		auto deltaTime = std::chrono::duration_cast<std::chrono::microseconds>(currentFrame - lastFrame).count();
 
-		fpsTimer += deltaTime;
-		timeSinceLastPhysicsTick += deltaTime;
-		fps++;
-
-		//Check if 1 second has passed
-		if (fpsTimer >= 1000000)
+		if (!pauseGame)
 		{
-			fpsTimer = 0;
-			fpsDisplay = fps;
-			fps = 0;
-			printf("fps: %d\n", fpsDisplay);
-		}
+			timeSinceLastPhysicsTick += deltaTime;
 
-		//Handle input
-		input_->ProcessInput();
-
-		//Check for collisions. This should be part of physics simulation. It is also very buggy and O(n^2)
-		for (const auto& gameobject : gameobjects_)
-		{
-			RigidbodyComponent* rb = gameobject.second->GetFirstComponentOfType<RigidbodyComponent>();
-			if (!rb)
-			{
-				continue;
-			}
+			//Check for collisions. This should be part of physics simulation. It is also very buggy and O(n^2)
 			for (const auto& gameobject : gameobjects_)
 			{
-				auto otherRb = gameobject.second->GetFirstComponentOfType<RigidbodyComponent>();
-				if (!otherRb)
+				RigidbodyComponent* rb = gameobject.second->GetFirstComponentOfType<RigidbodyComponent>();
+				if (!rb)
 				{
 					continue;
 				}
-				if (rb != otherRb)
+				for (const auto& gameobject : gameobjects_)
 				{
-					bool isCollided = rb->CheckCollision(*otherRb);
-					if (isCollided)
+					auto otherRb = gameobject.second->GetFirstComponentOfType<RigidbodyComponent>();
+					if (!otherRb)
 					{
-						otherRb->GetGameobject().OnCollision(*rb);
+						continue;
+					}
+					if (rb != otherRb)
+					{
+						bool isCollided = rb->CheckCollision(*otherRb);
+						if (isCollided)
+						{
+							otherRb->GetGameobject().OnCollision(*rb);
+						}
 					}
 				}
 			}
-		}
 
-		//Since the physics simulation is not tied to the framerate, we need to interpolate between the old and the new positions.
-		//The t value for interpolation is the subframe.
-		float subframe = timeSinceLastPhysicsTick / (float)microSecondsPerTick;
-		
-		//printf("Subframe %f\n", subframe);
-		//Check if its time to run physics
-		if (timeSinceLastPhysicsTick >= microSecondsPerTick)
-		{
-			printf("Physics Tick\n");
-			//run physics simulation
-			for (const auto& gameobject : gameobjects_)
+			//Since the physics simulation is not tied to the framerate, we need to interpolate between the old and the new positions.
+			//The t value for interpolation is the subframe.
+			subframe = timeSinceLastPhysicsTick / (float)microSecondsPerTick;
+
+			//printf("Subframe %f\n", subframe);
+			//Check if its time to run physics
+			if (timeSinceLastPhysicsTick >= microSecondsPerTick)
 			{
-				gameobject.second->OnUpdate(timeSinceLastPhysicsTick);
-				//gameobject.second->OnUpdate(microSecondsPerTick);
+				//run physics simulation
+				for (const auto& gameobject : gameobjects_)
+				{
+					gameobject.second->OnUpdate(timeSinceLastPhysicsTick);
+					//gameobject.second->OnUpdate(microSecondsPerTick);
+				}
+				timeSinceLastPhysicsTick = 0;
 			}
-			timeSinceLastPhysicsTick = 0;
-		}
 
-		//TODO ghetto fix. Or is it?
-		if (subframe > 1)
-		{
-			subframe = subframe - 1.f;
+			//TODO ghetto fix. Or is it?
+			if (subframe > 1)
+			{
+				subframe = subframe - 1.f;
+			}
 		}
-
 		//printf("subframe %f\n", subframe);
 		//Draw 
 		renderer_->BeginFrame();
@@ -113,11 +102,8 @@ void Engine::StartGame()
 			//gameobject.second->OnDraw(1);
 		}
 		{
-			static float f = 0.0f;
-			static int counter = 0;
-
 			ImGui::Begin("CodinaColada - Engine - Debug");                          // Create a window called "Hello, world!" and append into it.
-
+			ImGui::Checkbox("Pause", &pauseGame);
 			if (ImGui::TreeNode("GameObjects"))
 			{
 				for (auto& it : gameobjects_)
@@ -131,7 +117,6 @@ void Engine::StartGame()
 				}
 				ImGui::TreePop();
 			}
-
 			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 			ImGui::End();
 		}
